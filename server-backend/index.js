9e18f42f859e035c573b888bd7747ff8683b55af
Mac <imrehman.work@gmail.com>
2022-07-23 06:33:02 +0500

    connected mongodb successfully

---
+++ server-backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+
+require("dotenv").config()
+
+const app = express()
+const port = process.env.PORT
+
+app.use(cors())
+app.use(express.json())
+
+const uri = process.env.MONGO_URI
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const mongooseConnection = mongoose.connection
+mongooseConnection.once("open", () => {
+  console.log("MongoDB Connected Successfully!")
+})
+
+app.listen(port, () => {
+  console.log(`Server is running at port number = ${port}`)
+})
