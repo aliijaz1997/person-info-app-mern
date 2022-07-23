6de4f95e2dcee752ca684dcfe40ef7220a578850
Mac <imrehman.work@gmail.com>
2022-07-23 06:44:38 +0500

    added model schema

---
+++ server-backend/models/person-info.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema
+
+const personSchema = new Schema(
+  {
+    firstName: { type: String, required: true },
+    lastname: { type: String, required: true },
+    picture: { type: String, required: true },
+    profession: { type: String, required: true },
+    hairColor: { type: Number, required: true },
+    hairLength: { type: Number, required: true },
+    braSize: { type: Number, required: true },
+    waistSize: { type: Number, required: true },
+    height: { type: Number, required: true },
+    weight: { type: Number, required: true },
+    dateOfBirth: { type: Date, required: true },
+    castings: { type: [String], required: true }
+  },
+  {
+    timestamps: true
+  }
+)
+
+const Person = mongoose.model("Person", personSchema)
+
+module.exports = Person
